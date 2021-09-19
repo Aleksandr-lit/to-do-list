d92eee7ef5371a9408c7d45b138bb39ed422a926
Aleksandr-lit <53788620+Aleksandr-lit@users.noreply.github.com>
2021-09-19 13:42:16 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,41 @@
+
+let title = document.querySelector(".todolist__title");
+let box = document.querySelector(".todolist__box");
 let inputAdd = document.querySelector(".todolist__input-add");
 let todolistBody = document.querySelector(".todolist__body");
+let line = document.querySelector(".todolist__line");
+
+
+function loadAnimation() {
+    window.addEventListener("load", () => {
+        setTimeout(() => {
+            title.classList.add("_load");
+            box.classList.add("_load");
+            line.classList.add("_load");
+        }, 300);
+    });
+}
+
+function showPopap(txt, time) {
+    let popap = document.createElement("div");
+    popap.className = "popap";
+    popap.innerHTML = `<span class="popap__text">${txt}</span>`;
+    document.body.append(popap);
+    setTimeout(() => {
+        popap.classList.add("_active");
+    }, time);
+    setTimeout(() => {
+        popap.classList.remove("_active");
+    }, 2000);
+    setTimeout(() => {
+        popap.remove();
+    }, 2500);
+}
 
 function addDeleteElement() {
     inputAdd.addEventListener("change", function () {
         let text = inputAdd.value;
-        
         if(text.trim()) {
-    
             let item = document.createElement("li");
             item.className = "todolist__item";
             item.innerHTML = `<span class="todolist__item-text">${text}</span><button class="todolist__btn-shift btn">
@@ -14,40 +43,48 @@ function addDeleteElement() {
             inputAdd.value = "";
 
             let listTasks = document.querySelector(".todolist__list-tasks");
-            listTasks.append(item);
+            listTasks.prepend(item);
             
+            setTimeout(() => {
+                item.classList.add("_active");
+            }, 50);
+
             let btnDeletes = document.querySelectorAll(".todolist__btn-del");
             btnDeletes.forEach((elem) => {
                 elem.addEventListener("click", function () {
-                this.parentElement.remove(); 
+                    this.parentElement.classList.remove("_active");
+                    setTimeout(() => {
+                        this.parentElement.remove();
+                    }, 400);
+                     
                 });
             });
 
             let btnShift = document.querySelectorAll(".todolist__btn-shift");
             let listCompletedTask = document.querySelector(".todolist__list-completed-tasks");
+
             btnShift.forEach((elem) => {
                 elem.addEventListener("click", function () {
-                    listCompletedTask.append(this.parentElement);
-                    this.style.cssText = `background: #FFFFFF; right: 50px; transform: translate(-50%, -50%) scale(1.2);`;
+                    this.parentElement.classList.remove("_active");
+                    setTimeout(() => {
+                        this.parentElement.classList.add("_active");
+                    }, 400);
+                    setTimeout(() => {
+                        listCompletedTask.prepend(this.parentElement);
+                    }, 200);
                     this.previousSibling.style.color = "#FFFFFF";
-                    this.nextSibling.style.cssText = `background: #cb37b5; right: 20px;`;
                     this.parentElement.style.background = "#3a7bd5";
+                    showPopap(`Завдання виконано!`, 800);
                 });
             });
         } else {
-            let popap = document.createElement("div");
-            popap.className = "popap";
-            popap.innerHTML = `<span class="popap__text">Введіть будь-ласка завдання!</span>`;
-            document.body.append(popap);
-            setTimeout(() => {
-                popap.remove();
-            }, 3000);
+            showPopap("Введіть будь-ласка завдання!", 200);
             inputAdd.value = "";
-            // alert("Введіть будь-ласка завдання!");
         }
     });
 }
 
+loadAnimation();
 addDeleteElement();
 
 
